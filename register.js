f2219bc57cb8196ef30823eef83b8700457a293b
jethro rome k aniz <jethromeaniz@gmail.com>
2024-03-05 14:51:54 +0800

    Added a password and confirm password input field

--- register.js
+++ register.js
@@ -1,19 +1,35 @@
+//console.log("Hello World!", 1, 2, 3, true, ['apple', 'banana', 'orange']);
+//console.error("Hello World");
+//console.warn("Hello World");
+//console.table(['apple', 'banana', 'grape']);
 
+//let name = true
+//console.log(typeof(name))
 
-//string = "raniel", 'raniel" , "23"
-//number - 24, 24.1, 1230c
-
-//boolen - true, false, 1 or 0, = false, >
-
-
-
-
-let txtUsername = document.querySelector("#txtUsername");
-let btnRegister = document.querySelector("#btnRegister");
+let txtUsername = document.querySelector("#txtUsername")
+let txtPassword = document.querySelector("#txtPassword")
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword")
+let btnRegister = document.querySelector("#btnRegister")
 
 btnRegister.onclick = function () {
-    btnRegister(txtUsername.value)
+    register(txtUsername.value, txtPassword.value, txtConfirmPassword.value)
 }
-function register (username) {
-    console.log(username)
+
+function register (username, password, confirmpassword) {
+    //if ...,else if if...else
+    //Conditional Operators (Greater than >, Less than <, Equal ==, Not !, Or !!)
+    //Greater than or equal >=, Less than or Equal <=
+    //Not equal !=
+
+    console.log(username, password, confirmpassword)
+   
+    if(password == confirmpassword && password != "" && confirmpassword != ""){
+       return console,log("Password match")
+    } else if (password == ""|| confirmpassword ==""){
+
+    }
+    else {
+        return console.log("Password does not match.")
+    }
+    
 }
